ef01e508a7b055e5bf200f3ba8dc3b47101b99f6
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2024-10-22 23:17:07 +0200

    Add tests that we can parse current templates

---
+++ code/SiteGenerator.Tests/TemplateTests/TemplateProviderTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using HandlebarsDotNet;
+using SiteGenerator.Templates;
+using Xunit;
+
+namespace SiteGenerator.Tests.TemplateTests;
+
+public class FileTemplateProviderTests
+{
+    private readonly string _testTemplatePath = Path.Combine("TestData", "templates");
+
+    [Fact]
+    public void GetTemplateContent_ShouldReadNoteTemplate()
+    {
+        // Arrange
+        var provider = new FileTemplateProvider(_testTemplatePath);
+        var templateName = "note";
+
+        // Act
+        var templateContent = provider.GetTemplateContent(templateName);
+
+        // Assert
+        templateContent.Should().NotBeNullOrEmpty();
+        var compiledTemplate = Handlebars.Compile(templateContent);
+        compiledTemplate.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void GetTemplateContent_ShouldReadLayoutTemplate()
+    {
+        // Arrange
+        var provider = new FileTemplateProvider(_testTemplatePath);
+        var templateName = "layout";
+
+        // Act
+        var templateContent = provider.GetTemplateContent(templateName);
+
+        // Assert
+        templateContent.Should().NotBeNullOrEmpty();
+        var compiledTemplate = Handlebars.Compile(templateContent);
+        compiledTemplate.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void GetPartialContent_ShouldReadHeadPartial()
+    {
+        // Arrange
+        var provider = new FileTemplateProvider(_testTemplatePath);
+        var partialName = "head";
+
+        // Act
+        var partialContent = provider.GetPartialContent(partialName);
+
+        // Assert
+        partialContent.Should().NotBeNullOrEmpty();
+        Handlebars.RegisterTemplate(partialName, partialContent);
+        var template = Handlebars.Compile("{{> head}}");
+        template.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void GetPartialContent_ShouldReadBacklinksPartial()
+    {
+        // Arrange
+        var provider = new FileTemplateProvider(_testTemplatePath);
+        var partialName = "backlinks";
+
+        // Act
+        var partialContent = provider.GetPartialContent(partialName);
+
+        // Assert
+        partialContent.Should().NotBeNullOrEmpty();
+        Handlebars.RegisterTemplate(partialName, partialContent);
+        var template = Handlebars.Compile("{{> backlinks}}");
+        template.Should().NotBeNull();
+    }
+}
