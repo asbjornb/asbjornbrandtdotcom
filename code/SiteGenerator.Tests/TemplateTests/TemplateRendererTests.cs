a22561e3f2ac08aec0aa04577ad0b1d9f748db06
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2024-10-23 00:24:46 +0200

    Add simple test for note renderer

---
+++ code/SiteGenerator.Tests/TemplateTests/TemplateRendererTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using HandlebarsDotNet;
+using SiteGenerator.Templates;
+using SiteGenerator.Templates.MetadataModels;
+using Xunit;
+
+namespace SiteGenerator.Tests.TemplateTests;
+
+public class TemplateRendererTests
+{
+    private readonly string _testTemplatePath = Path.Combine("TestData", "templates");
+
+    [Fact]
+    public void RenderTemplateWithoutBacklinks_ShouldRenderNoteTemplate()
+    {
+        // Arrange
+        var provider = new FileTemplateProvider(_testTemplatePath);
+        var renderer = new TemplateRenderer(provider);
+
+        /// Use a multiline markdown string with a header and a single line
+        var simpleNoteContent =
+            @"# Simple Note
+This is a simple note.";
+        var noteData = new NoteModel(simpleNoteContent, []);
+        var layoutData = new LayoutModel(
+            "SomeTitle",
+            "SomeDescription",
+            "Website",
+            "SomeUrl",
+            null
+        );
+
+        // Act
+        var renderedTemplate = renderer.RenderNote(noteData, layoutData);
+
+        // Assert
+        renderedTemplate.Should().NotBeNullOrEmpty();
+    }
+}
--- code/SiteGenerator/Templates/MetadataModels/LayoutModel.cs
+++ code/SiteGenerator/Templates/MetadataModels/LayoutModel.cs
@@ -3,8 +3,7 @@
 public record LayoutModel(
     string MetaTitle,
     string MetaDescription,
-    string MetaType,
+    string MetaType, //MetaType can be "website" or "article". Check OpenGraph protocol for more information.
     string PageUrl,
-    string SiteAuthor,
     string? Body
 );
