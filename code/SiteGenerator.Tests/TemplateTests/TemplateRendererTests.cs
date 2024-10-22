dc7d52ca9b851ec52f475b5a46acccdf35097461
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2024-10-23 00:28:55 +0200

    Add more assertions

--- code/SiteGenerator.Tests/TemplateTests/TemplateRendererTests.cs
+++ code/SiteGenerator.Tests/TemplateTests/TemplateRendererTests.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using HandlebarsDotNet;
 using SiteGenerator.Templates;
 using SiteGenerator.Templates.MetadataModels;
 using Xunit;
@@ -17,10 +16,7 @@ public class TemplateRendererTests
         var provider = new FileTemplateProvider(_testTemplatePath);
         var renderer = new TemplateRenderer(provider);
 
-        /// Use a multiline markdown string with a header and a single line
-        var simpleNoteContent =
-            @"# Simple Note
-This is a simple note.";
+        var simpleNoteContent = "<h1>Simple Note</h1><p>This is a simple note.</p>";
         var noteData = new NoteModel(simpleNoteContent, []);
         var layoutData = new LayoutModel(
             "SomeTitle",
@@ -35,5 +31,17 @@ This is a simple note.";
 
         // Assert
         renderedTemplate.Should().NotBeNullOrEmpty();
+
+        // Check for the presence of key HTML elements
+        renderedTemplate.Should().Contain("<html");
+        renderedTemplate.Should().Contain("<head>");
+        renderedTemplate.Should().Contain("<title>SomeTitle</title>");
+        renderedTemplate.Should().Contain("<body>");
+        renderedTemplate.Should().Contain("<h1>Simple Note</h1>");
+        renderedTemplate.Should().Contain("<p>This is a simple note.</p>");
+        renderedTemplate.Should().Contain("Copyright Asbjørn Brandt");
+
+        // Check that backlinks are not present
+        renderedTemplate.Should().NotContain("<ul class=\"backlinks-container\">");
     }
 }
